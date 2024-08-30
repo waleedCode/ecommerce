f49a8f1b7341581d8bc9fcb44057bfd65ac979e7
waleedcode <waleedflutter@gmail.com>
2024-08-30 11:08:27 +0300

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin user',
+                'username' => 'adminuser',
+                'email' => 'admin@gmail.com',
+                'role' => 'admin',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => 'vendor user',
+                'username' => 'vendor',
+                'email' => 'vendor@gmail.com',
+                'role' => 'vendor',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ],
+            [
+                'name' => 'user user',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'role' => 'user',
+                'status' => 'active',
+                'password' => bcrypt('password')
+            ]
+        ]);
+    }
+}
